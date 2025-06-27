054f3d66c77377cdfaecd2426f5cc14b0938c927
Moamen771 <memoaser86@gmail.com>
2025-06-27 22:44:54 +0300

    create custom text form field

---
+++ lib/features/auth/ui/views/widgets/custom_text_form_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../../../../../core/utlis/app_colors.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    super.key,
+    required this.text,
+    required this.hintText,
+    required this.obscureText,
+    this.suffixIcon,
+    this.prefixIcon,
+  });
+
+  final String text;
+  final String hintText;
+  final bool obscureText;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 8,
+      children: [
+        Text(
+          text,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+        ),
+        TextFormField(
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: const TextStyle(color: AppColors.darkGrey),
+            filled: true,
+            fillColor: AppColors.lightGrey,
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide.none,
+            ),
+            suffixIcon: suffixIcon,
+            prefixIcon: prefixIcon,
+          ),
+        ),
+      ],
+    );
+  }
+}
