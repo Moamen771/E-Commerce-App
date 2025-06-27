7ad7dedc65de9f7450b3da17739b02670a4ddb9a
Moamen771 <memoaser86@gmail.com>
2025-06-27 20:42:30 +0300

    create onboarding view

---
+++ lib/features/auth/ui/views/onboarding_view.dart
@@ -0,0 +1,11 @@
+import 'package:ecommerce/features/auth/ui/views/widgets/onboarding_body.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingView extends StatelessWidget {
+  const OnboardingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: OnboardingBody());
+  }
+}
