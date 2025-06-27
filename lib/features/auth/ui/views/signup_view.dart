e3bcd714e804272cf9eb9b5a690600e36c1646f9
Moamen771 <memoaser86@gmail.com>
2025-06-27 22:11:11 +0300

    create signup view

---
+++ lib/features/auth/ui/views/signup_view.dart
@@ -0,0 +1,11 @@
+import 'package:ecommerce/features/auth/ui/views/widgets/signup_body.dart';
+import 'package:flutter/material.dart';
+
+class SignupView extends StatelessWidget {
+  const SignupView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: SignupBody());
+  }
+}
