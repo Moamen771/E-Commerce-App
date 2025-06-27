69683073b02ecbcd5c63614ee4748e2b44f574c3
Moamen771 <memoaser86@gmail.com>
2025-06-27 20:42:06 +0300

    create app colors

---
+++ lib/core/utlis/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const Color orange = Color(0xffEC5500);
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffffffff);
+}
