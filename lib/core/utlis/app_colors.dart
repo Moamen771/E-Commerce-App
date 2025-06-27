21fc1aedc6cdd82f84cea8aced4df4ca13f16cf2
Moamen771 <memoaser86@gmail.com>
2025-06-27 22:12:10 +0300

    add grey colors to app color

--- lib/core/utlis/app_colors.dart
+++ lib/core/utlis/app_colors.dart
@@ -4,4 +4,6 @@ abstract class AppColors {
   static const Color orange = Color(0xffEC5500);
   static const Color black = Color(0xff000000);
   static const Color white = Color(0xffffffff);
+  static const Color lightGrey = Color(0xffF5F5F5);
+  static const Color darkGrey = Color(0xff666666);
 }
