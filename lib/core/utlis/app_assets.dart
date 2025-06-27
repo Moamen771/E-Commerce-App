f7e43cda385e18bb76adea06d859fad7740fecda
Moamen771 <memoaser86@gmail.com>
2025-06-27 20:41:51 +0300

    create app assets

---
+++ lib/core/utlis/app_assets.dart
@@ -0,0 +1,7 @@
+abstract class AppAssets {
+  static const String splashLogo = 'assets/splash_logo.png';
+  static const String onboardingLogo =
+      'assets/onboarding_logo.png';
+  static const String onboardingImage =
+      'assets/onboarding_image.png';
+}
