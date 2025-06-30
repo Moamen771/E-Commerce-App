d012ba068324879167c8c624c1d3d458ac131cae
Moamen771 <memoaser86@gmail.com>
2025-06-30 20:20:31 +0300

    refactor app colors

--- lib/core/utlis/app_colors.dart
+++ lib/core/utlis/app_colors.dart
@@ -5,5 +5,6 @@ abstract class AppColors {
   static const Color black = Color(0xff000000);
   static const Color white = Color(0xffffffff);
   static const Color lightGrey = Color(0xffF5F5F5);
-  static const Color darkGrey = Color(0xff666666);
+  static const Color grey = Color(0xff666666);
+  static const Color darkGrey = Color(0xff5B5B5B);
 }
