b22f1bbb7449b2a02c0074bd32db37695a6b9277
Moamen771 <memoaser86@gmail.com>
2025-07-03 16:29:27 +0300

    add app space class

--- lib/core/helpers/spacing.dart
+++ lib/core/helpers/spacing.dart
@@ -3,3 +3,13 @@ import 'package:flutter/cupertino.dart';
 SizedBox verticalSpace(double height) => SizedBox(height: height);
 
 SizedBox horizontalSpace(double width) => SizedBox(width: width);
+
+class AppSpace {
+  static const double s4 = 4.0;
+  static const double s8 = 8.0;
+  static const double s16 = 16.0;
+  static const double s24 = 24.0;
+  static const double s32 = 32.0;
+  static const double s48 = 48.0;
+  static const double s64 = 64.0;
+}
