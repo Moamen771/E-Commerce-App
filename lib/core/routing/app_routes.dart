3388ae6b50694a02789a214db84e772c8a84a09b
Moamen771 <memoaser86@gmail.com>
2025-07-03 16:58:13 +0300

    change route to onboarding & add forget password

--- lib/core/routing/app_routes.dart
+++ lib/core/routing/app_routes.dart
@@ -1,5 +1,6 @@
 abstract class AppRoutes {
-  static const String initialRoute = '/';
+  static const String onboardingRoute = '/onboarding';
   static const String signupRoute = '/signUp';
   static const String loginRoute = '/login';
+  static const String forgotPasswordRoute = '/forgotPassword';
 }
