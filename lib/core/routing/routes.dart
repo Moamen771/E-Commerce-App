1e0a576340aba69889d2c751885c1fd7784870b7
Moamen771 <memoaser86@gmail.com>
2025-07-03 16:58:35 +0300

    add forget password

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/features/auth/ui/views/forget_password_view.dart';
 import 'package:flutter/material.dart';
 import '../../../features/auth/ui/views/login_view.dart';
 import '../../../features/auth/ui/views/onboarding_view.dart';
@@ -7,12 +8,16 @@ import 'app_routes.dart';
 class Routes {
   static Route onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
-      case AppRoutes.initialRoute:
+      case AppRoutes.onboardingRoute:
         return MaterialPageRoute(builder: (context) => const OnboardingView());
       case AppRoutes.signupRoute:
         return MaterialPageRoute(builder: (context) => const SignupView());
       case AppRoutes.loginRoute:
         return MaterialPageRoute(builder: (context) => const LoginView());
+      case AppRoutes.forgotPasswordRoute:
+        return MaterialPageRoute(
+          builder: (context) => const ForgetPasswordView(),
+        );
       default:
         return MaterialPageRoute(
           builder: (context) =>
